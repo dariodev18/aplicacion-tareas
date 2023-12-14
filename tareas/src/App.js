5e279570f15d011f71ecf9161d878d11eb1093dd
dariodev18 <dariodev18@users.noreply.github>
2023-12-14 12:52:43 -0300

    inicio proyecto tareas

---
+++ tareas/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import freeCodeCampLogo from './imagenes/freecodecamp-logo.png'
+
+function App() {
+  return (
+    <div className='App'>
+      <div className='freecodecamp-logo-contenedor'>
+        <img 
+        src={freeCodeCampLogo}
+        className='freecodecamp-logo'
+        alt='logo de freeCodeCamp' />
+      </div>
+    </div>
+  );
+}
+
+export default App;
